69b8f365f4c969bedf527878b6f9ecd07acabe6f
CL_Dawn <14636198+cldawn@user.noreply.gitee.com>
2025-03-20 19:05:43 +0800

    fifth homework

---
+++ assignment5/assignment5/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace assignment5
+{
+    internal class Program
+    {
+        public class Node<T>
+        {
+            public Node<T> Next { get; set; }
+            public T Value { get; set; }
+            public Node(T value) { Value = value;Next = null;}
+
+        }
+        public class GenericList<T>
+        {
+            public Node<T> head;
+            public Node<T> tail;
+            public GenericList()
+            {
+                tail = head = null;
+            }
+            public Node<T> Head { get => head; }
+            public void Add(T value)
+            {
+                Node<T> n=new Node<T>(value);
+                if(tail==null)
+                {
+                    head=tail = n;
+                }
+                else
+                {
+                    tail.Next = n;
+                    tail = n;
+                }
+            }
+            public void ForEach(Action<T> action)
+            {
+                Node<T> node = head;
+                while(node != null)
+                {
+                    action.Invoke(head.Value);
+                    node = node.Next;
+                }
+
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            GenericList<int>list= new GenericList<int>();
+            list.Add(1);
+            list.Add(2);
+            list.Add(3);
+            list.Add(4);
+            list.Add(5);
+            list.Add(6);
+            list.ForEach(x=>Console.WriteLine(x));
+            int max=list.head.Value;
+            list.ForEach(x => { if (x > max) max = x; });
+            int min=list.head.Value;    
+            list.ForEach(x=> { if (x < min) min = x; });
+
+
+        }
+    }
+}
---
+++ assignment5/homework2/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Timers;
+
+namespace homework2
+{
+    internal class Program
+    {
+        public delegate void TickHandler(object sender, TickEventArgs args);
+        public class TickEventArgs
+        {
+            public  string DiDaText { get; set; }
+            public TickEventArgs(string diDaText) {  DiDaText = diDaText; }
+            public TickEventArgs() { DiDaText = "dida!dida!"; }
+        }
+
+        public delegate void AlarmHandler(object sender, AlarmEventArgs args);
+
+        public class AlarmEventArgs
+        {
+            public string AlarmText { get; set; }
+            public AlarmEventArgs(string alarmText) {  AlarmText = alarmText; }
+            public AlarmEventArgs() { AlarmText = "Alarm!"; }
+
+        }
+        public class Clock
+        {
+            public event TickHandler Tick;
+            public event AlarmHandler Alarm;
+            public bool stop=false;
+            private int _alarmTime;
+            private int _currentTime;
+            public void SubscribeEvents()
+            {
+                Tick += Clock_Tick;
+                Alarm += Clock_Alarm;
+            }
+
+            private void Clock_Tick(object sender, TickEventArgs args)
+            {
+                Console.WriteLine(args.DiDaText);
+            }
+
+            private void Clock_Alarm(object sender, AlarmEventArgs args)
+            {
+                Console.WriteLine(args.AlarmText);
+            }
+
+            public Clock(int alarmTime) 
+            { 
+                SubscribeEvents();
+                _alarmTime = alarmTime; 
+            }
+            public void StartClock()
+            {
+                _currentTime = 0;
+                while(!stop)
+                {
+                    if (_currentTime < _alarmTime)
+                    {
+                        Thread.Sleep(1000);
+                        _currentTime++;
+                        OnTick();
+                    }
+                    OnAlarm();
+                }
+
+            }
+
+            private void OnAlarm()
+            {
+               AlarmEventArgs args = new AlarmEventArgs();
+                Alarm.Invoke(this, args);
+            }
+
+            private void OnTick()
+            {
+                TickEventArgs args = new TickEventArgs();
+                Tick.Invoke(this, args);
+            }
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
